8dd0c0009559c4b4532c4083945b0d6f8751eba2
LaylaRP <maroonrparker@gmail.com>
2025-12-12 08:35:35 +0000

    atualizar app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Configurações do Supabase - SUBSTITUA COM SEUS DADOS
-const SUPABASE_URL = 'https://seu-projeto.supabase.co'; // Substitua pela sua URL
-const SUPABASE_ANON_KEY = 'sua-chave-anon-publica'; // Substitua pela sua chave
+const SUPABASE_URL = 'https://mpbuejsxiltcfwfnqpfe.supabase.co'; // Substitua pela sua URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wYnVlanN4aWx0Y2Z3Zm5xcGZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MDYwMzcsImV4cCI6MjA4MTA4MjAzN30.N2802Orv1LLDJ5hOpbn_zO8tKfKHsSY_hPBx68BX42c'; // Substitua pela sua chave
 
 // Configurações locais
 const STORAGE_USER_KEY = 'jupterNewsUser';
